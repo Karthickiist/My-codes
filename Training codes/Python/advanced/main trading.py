234c96347c472889d74be1da75395a21e4a34106
Karthickiist <110439574+Karthickiist@users.noreply.github.com>
2022-09-24 21:40:58 -0400

    Delete Training codes/Python/advanced directory

--- Training codes/Python/advanced/main trading.py
+++
@@ -1,6 +0,0 @@
-from trading import trading
-a=trading("Sam")
-b=trading("Ron")
-
-a.buy(1000,15)
-a.display()
--- Training codes/Python/advanced/module test.py
+++
@@ -1,4 +0,0 @@
-from shapes.circle import area
-
-a=float(input("Enter the radius of the circle: "))
-print("The area is",area(a))
