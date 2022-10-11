6ae382098352c46700e952c5275f1f9a2ed73884
Karthickiist <110439574+Karthickiist@users.noreply.github.com>
2022-10-10 22:33:26 -0400

    Delete Python_projects/Python projects directory

--- Python_projects/Python projects/quiz/question_model.py
+++
@@ -1,4 +0,0 @@
-class questions:
-    def __init__(self,r_text,r_answer):
-        self.text=r_text
-        self.answer=r_answer
--- Python_projects/Python projects/rain_alert_app/main.py
+++
@@ -1,5 +0,0 @@
-import requests
-api_key='dfb45fe9a8c9a1a22dd4c1a0709089c8'
-
-data=requests.get(url='https://api.openweathermap.org/data/2.5/weather?lat=43.653225&lon=-79.383186&appid=dfb45fe9a8c9a1a22dd4c1a0709089c8')
-print(data.json())
